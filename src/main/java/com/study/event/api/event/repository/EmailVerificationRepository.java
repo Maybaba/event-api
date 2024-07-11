d1e4a30d94fa0a9ae14c13383b4d2fd81f61ac9e
Maybaba <sinyunjong@gmail.com>
2024-07-11 11:45:03 +0900

    # email sign-up set

--- src/main/java/com/study/event/api/event/repository/EmailVerificationRepository.java
+++ src/main/java/com/study/event/api/event/repository/EmailVerificationRepository.java
@@ -1,8 +1,15 @@
 package com.study.event.api.event.repository;
 
 import com.study.event.api.event.entity.EmailVerification;
+import com.study.event.api.event.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
+
+    //유저 정보를 통해 인증정보를 탐색
+    // 정 불안하다 싶으면 쿼리 DSL로 하면 된ㄷ.
+    Optional<EmailVerification> findByEventUser(EventUser eventUser);
 
 }
