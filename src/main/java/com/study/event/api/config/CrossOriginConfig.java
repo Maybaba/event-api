480cc5fafa1bc207cdc06cfef2ed62dacef40c93
Maybaba <sinyunjong@gmail.com>
2024-07-12 10:33:48 +0900

    인증코드 사용자피드백 기능

--- src/main/java/com/study/event/api/config/CrossOriginConfig.java
+++ src/main/java/com/study/event/api/config/CrossOriginConfig.java
@@ -1,10 +1,12 @@
 package com.study.event.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // global cross origin setting
+@Configuration
 public class CrossOriginConfig implements WebMvcConfigurer {
 
     private String[] urls = {
